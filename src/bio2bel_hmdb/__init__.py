7c4f634e4d4bcccd5b109354ffa9d91904e663c7
Colin Birkenbihl <colin.birken@gmail.com>
2017-09-01 14:49:48 +0200

    - added __init__.py - added travis - added requirements - added setup.py - added folder structure

---
+++ src/bio2bel_hmdb/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.1.1-dev'
+
+__title__ = 'bio2bel_hmdb'
+__description__ = "A package for converting the Human Metabolome Database (HMDB) to BEL."
+__url__ = 'https://github.com/bio2bel/hmdb'
+
+__author__ = 'Colin Birkenbihl, Charles Tapley Hoyt'
+__email__ = 'charles.hoyt@scai.fraunhofer.de'
+
+__license__ = 'Apache 2.0 License'
+__copyright__ = 'Copyright (c) 2017 Colin Birkenbihl, Charles Tapley Hoyt'
