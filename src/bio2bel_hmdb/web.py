435ad2b3ad131b73bed794d32f43252bc97afd8b
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-11-25 16:58:40 +0100

    Update web construction

--- src/bio2bel_hmdb/web.py
+++ src/bio2bel_hmdb/web.py
@@ -8,15 +8,28 @@ from flask_admin.contrib.sqla import ModelView
 from bio2bel_hmdb.manager import Manager
 from bio2bel_hmdb.models import *
 
-app = Flask(__name__)
-admin = flask_admin.Admin(app, url='/')
 
-manager = Manager()
+def add_admin(app, session, url=None):
+    admin = flask_admin.Admin(app, url=(url or '/'))
+    admin.add_view(ModelView(Metabolite, session))
+    admin.add_view(ModelView(Pathways, session))
+    admin.add_view(ModelView(Diseases, session))
+    admin.add_view(ModelView(Proteins, session))
+    return admin
 
-admin.add_view(ModelView(Metabolite, manager.session))
-admin.add_view(ModelView(Pathways, manager.session))
-admin.add_view(ModelView(Diseases, manager.session))
-admin.add_view(ModelView(Proteins, manager.session))
+
+def create_app(connection=None, url=None):
+    """Creates a Flask application
+
+    :type connection: Optional[str]
+    :rtype: flask.Flask
+    """
+    app = Flask(__name__)
+    manager = Manager(connection=connection)
+    add_admin(app, manager.session, url=url)
+    return app
+
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True, host='0.0.0.0', port=5000)
